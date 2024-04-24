101881aa7370b0ee1d44469323d6778165fa4729
ecpvt <camme52prisacari06ecaterina@gmail.com>
2024-04-24 09:28:58 +0300

    stergerea mapei vechi

--- git.hub.webaap.acdy/js.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Simulăm o întârziere de 2 secunde pentru a demonstra pagina de încărcare
-  setTimeout(function() {
-    document.querySelector('.loader-wrapper').style.display = 'none';
-  }, 2000);
-});
